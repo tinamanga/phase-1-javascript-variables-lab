4c55faef3e810b3cb39e015220a0443e5b029879
tinamanga <christinamanga28@gmail.com>
2025-03-15 01:11:20 +0300

    creating variables

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+// Define companyName as a constant
+const companyName = 'Scuber';
+
+// Define mostProfitableNeighborhood using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define companyCeo using let
+let companyCeo = 'Susan Smith';
